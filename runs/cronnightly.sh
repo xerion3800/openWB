584ee8e8c088fa6b3956c62961120489ee97ac9b
Frank <frankortmann80@gmail.com>
2021-12-14 12:20:34 +0100

    fix stalled cronnightly.sh

--- runs/cronnightly.sh
+++ runs/cronnightly.sh
@@ -126,9 +126,9 @@ if [[ $verbraucher2_typ == "tasmota" ]]; then
 	fi
 fi
 
-curl -s https://raw.githubusercontent.com/snaptec/openWB/master/web/version >/var/www/html/openWB/ramdisk/vnightly
-curl -s https://raw.githubusercontent.com/snaptec/openWB/beta/web/version >/var/www/html/openWB/ramdisk/vbeta
-curl -s https://raw.githubusercontent.com/snaptec/openWB/stable/web/version >/var/www/html/openWB/ramdisk/vstable
+curl --connect-timeout 10 -s https://raw.githubusercontent.com/xerion3800/openWB/master/web/version >/var/www/html/openWB/ramdisk/vnightly
+curl --connect-timeout 10 -s https://raw.githubusercontent.com/xerion3800/openWB/beta/web/version >/var/www/html/openWB/ramdisk/vbeta
+curl --connect-timeout 10 -s https://raw.githubusercontent.com/xerion3800/openWB/stable/web/version >/var/www/html/openWB/ramdisk/vstable
 
 if [[ -s /var/www/html/openWB/ramdisk/randomSleepValue ]]; then
 	randomSleep=$(</var/www/html/openWB/ramdisk/randomSleepValue)
