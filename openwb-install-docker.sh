34bf8e6677dde9c2079b02fb162a5c37574f19a1
Frank <frankortmann80@gmail.com>
2023-03-16 19:53:53 +0100

    WIP

--- openwb-install-docker.sh
+++ openwb-install-docker.sh
@@ -187,12 +187,12 @@ fi
 echo "installing pymodbus"
 sudo pip install  -U pymodbus
 
-echo "check for paho-mqtt"
-if python3 -c "import paho.mqtt.publish as publish" &> /dev/null; then
-	echo 'mqtt installed...'
-else
-    sudo pip3 install paho-mqtt
-fi
+# echo "check for paho-mqtt"
+# if python3 -c "import paho.mqtt.publish as publish" &> /dev/null; then
+# 	echo 'mqtt installed...'
+# else
+#     sudo pip3 install paho-mqtt
+# fi
 
 #Adafruit install
 # echo "check for MCP4725"
